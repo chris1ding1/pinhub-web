cb58c0cf188d3617cdaa8538da8271d1dc3d1d2f
chris <chris1ding1@gmail.com>
2025-06-17 15:41:59 +0800

    tailwind css config.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxtjs/i18n'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   i18n: {
     defaultLocale: 'en',
-  }
+  },
 })
