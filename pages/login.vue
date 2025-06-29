2f21a8f29785c8472a7e6faa7aad12931e4d9123
chris <chris1ding1@gmail.com>
2025-06-29 14:45:39 +0800

    Dev Auth (Send email verify code and Auth Login)

--- pages/login.vue
+++ pages/login.vue
@@ -1,40 +1,81 @@
 <template>
   <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-    <form  method="POST" @submit="handleLoginFormSubmit">
-      <input v-model="email" type="email" name="email" placeholder="Email" required maxlength="255">
-      <input v-model="verifyCode" type="text" name="verify-code" required maxlength="6">
+    <form method="POST">
+      <input v-model="authEmailCodeForm.email" type="email" name="email" placeholder="Email" required maxlength="255">
+      <input v-model="authEmailCodeForm.verifyCode" type="text" name="verify-code" placeholder="Verify Code" required maxlength="6">
       <button type="button" @click="handleAuthEmailSend">Send Code</button>
-      <button type="submit">Login</button>
+      <button type="button" @click="handleLoginFormSubmit">Login</button>
     </form>
   </div>
 </template>
 <script setup lang="ts">
+import * as v from 'valibot';
+
 const config = useRuntimeConfig()
 const apiBase = config.public.apiBase as string
 
-const email = ref('')
-const verifyCode = ref('')
+
+const authEmailCodeForm = reactive({
+  email: '',
+  verifyCode: '',
+})
+
+const emailValidation = v.pipe(
+  v.string('Your email must be a string.'),
+  v.nonEmpty('Please enter your email.'),
+  v.maxLength(255),
+  v.email('The email address is badly formatted.'),
+)
+
+const verifyCodeValidation = v.pipe(
+  v.string(),
+  v.nonEmpty('Please enter verify code.'),
+)
+
+const SendAuthEmailCodeSchema = v.object({
+  email: emailValidation
+})
+
+const AuthEmailCodeSchema = v.object({
+  email: emailValidation,
+  verifyCode: verifyCodeValidation
+})
+
+type SendAuthEmailCodeData = v.InferOutput<typeof SendAuthEmailCodeSchema>
+type AuthEmailCodeData = v.InferOutput<typeof AuthEmailCodeSchema>
 
 async function handleAuthEmailSend() {
-  const res = await $fetch('auth/email/send', {
-    baseURL: apiBase,
-    method: 'POST',
-    body: {
-      email: email.value,
-    }
-  })
-  console.log(res)
+  try {
+    const validatedData = v.parse(SendAuthEmailCodeSchema, {
+      email: authEmailCodeForm.email
+    })
+
+    const res = await $fetch('auth/email/send', {
+      baseURL: apiBase,
+      method: 'POST',
+      body: validatedData
+    })
+    console.log(res)
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function handleLoginFormSubmit() {
-  const res = await $fetch('auth/email/verify', {
-    baseURL: apiBase,
-    method: 'POST',
-    body: {
-      email: email.value,
-      verifyCode: verifyCode.value,
-    }
-  })
-  console.log(res)
+  try {
+    const validatedData = v.parse(AuthEmailCodeSchema, {
+      email: authEmailCodeForm.email,
+      verifyCode: authEmailCodeForm.verifyCode
+    })
+
+    const res = await $fetch('auth/email/verify', {
+      baseURL: apiBase,
+      method: 'POST',
+      body: validatedData
+    })
+    console.log(res)
+  } catch (error) {
+    console.log(error);
+  }
 }
 </script>
