b01f5e0aaf26d1c3ba7ace81ba6bff63239a0880
chris <chris1ding1@gmail.com>
2025-06-29 23:56:44 +0800

    Add nuxt-auth-utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/i18n'],
+  modules: ['@nuxt/eslint', '@nuxtjs/i18n', 'nuxt-auth-utils'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
