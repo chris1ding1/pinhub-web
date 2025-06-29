ad1189b80fb7453fd79ab675bdb4fce69a44f12c
chris <chris1ding1@gmail.com>
2025-06-30 01:23:53 +0800

    login status.

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    Header
-    <button @click="logout">Logout</button>
-  </div>
+  <header class="bg-white">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex flex-1 justify-end">
+        <div v-if="isShowLogin" >
+          <a href="/login" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+        </div>
+        <div v-if="loggedIn" class="space-x-4">
+          <span class="text-sm/6 font-semibold text-gray-900">{{ user.name }}</span>
+          <button @click="logout" class="text-sm/6 font-semibold text-gray-900">Logout</button>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
 <script setup lang="ts">
 const { loggedIn, user, clear: clearSession } = useUserSession()
+const route = useRoute()
+
+const isShowLogin = computed(() => {
+  return !loggedIn.value && route.path !== '/login'
+})
 
 async function logout() {
   await clearSession()
+  await navigateTo('/')
 }
 </script>
