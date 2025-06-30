b9737e6bcd584249d7949cf1710b7af202008b63
chris <chris1ding1@gmail.com>
2025-06-30 18:38:46 +0800

    title size

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
     <header class="mx-auto max-w-3xl flex items-center justify-center mt-14">
-      <h1 class="text-3xl font-extrabold md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+      <h1 class="text-5xl font-extrabold md:text-6xl lg:text-7xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
         {{ title }}
       </h1>
     </header>
