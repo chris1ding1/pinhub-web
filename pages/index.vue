191b0ce9f0c6d28aac297a488eea9b0ad815f8dc
chris <chris1ding1@gmail.com>
2025-07-06 02:37:58 +0800

    update pins list style.

--- pages/index.vue
+++ pages/index.vue
@@ -10,24 +10,40 @@
     </header>
     <main>
         <PinsAppCreateModal />
-        <ul v-if="userPins?.data?.items?.length" role="list" class="grid grid-cols-2 gap-x-6 gap-y-8 lg:grid-cols-4 xl:gap-x-8 my-10">
-            <li v-for="pin in userPins.data.items" :key="pin.id" class="h-full">
-                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm h-full">
-                    <p class="mb-3 font-normal text-gray-500 line-clamp-3 overflow-hidden text-ellipsis">
-                        {{ pin.content || (pin.url ? pin.url : "") }}
-                    </p>
-                    <a
-                        v-if="pin.url"
-                        :href="pin.url"
-                        target="_blank"
-                        rel="nofollow noopener"
-                        class="inline-flex font-medium items-center text-blue-600 hover:underline"
-                    >
-                        Link
-                        <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
-                        </svg>
-                    </a>
+        <ul v-if="userPins?.data?.items?.length" role="list" class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8 xl:gap-x-8 my-10">
+            <li v-for="pin in userPins.data.items" :key="pin.id">
+                <div
+                    v-if="isImageOnly(pin)"
+                >
+                    <img :src="pin.image_url" class="rounded-lg pointer-events-none aspect-10/7 object-cover group-hover:opacity-75">
+                </div>
+                <div v-else class="border border-gray-200 rounded-2xl shadow-sm p-6">
+                    <div class="flex items-start gap-4">
+                        <div class="flex-1 min-w-0">
+                            <p
+                                class="truncate text-sm font-medium text-gray-900 line-clamp-3"
+                            >
+                                {{ pin.content || (pin.url ? pin.url : "") }}
+                            </p>
+                            <a
+                                v-if="pin.url"
+                                :href="pin.url"
+                                :title="pin.url"
+                                target="_blank"
+                                rel="nofollow noopener"
+                                class="truncate text-sm text-gray-500"
+                            >
+                                {{ pin.url_host }}
+                            </a>
+                        </div>
+                        <div v-if="pin.image_url" class="flex-none w-full sm:w-1/2">
+                            <img
+                                v-if="pin.image_url"
+                                :src="pin.image_url"
+                                class="rounded-lg pointer-events-none aspect-10/7 object-cover group-hover:opacity-75"
+                            >
+                        </div>
+                    </div>
                 </div>
             </li>
         </ul>
@@ -66,4 +82,8 @@ watch(loggedIn, (isLoggedIn) => {
     clear()
   }
 })
+
+const isImageOnly = (pin: any) => {
+  return pin.image_url && !pin.content && !pin.url
+}
 </script>
