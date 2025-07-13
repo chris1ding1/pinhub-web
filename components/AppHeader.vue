05cc466668e49cbf4f68e0bdbfea0df26b69f281
chris <chris1ding1@gmail.com>
2025-07-13 12:30:32 +0800

    login in a. inline-block

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,7 +3,7 @@
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="flex flex-1 justify-end">
         <div v-if="isShowLogin" >
-          <a href="/login" class="rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900">Log in <span aria-hidden="true">&rarr;</span></a>
+          <a href="/login" class="inline-block rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900">Log in <span aria-hidden="true">&rarr;</span></a>
         </div>
         <div v-if="loggedIn" class="space-x-4">
           <span class="text-sm/6 font-semibold text-gray-900">{{ user.name }}</span>
