697aec01f45ff1e4f5e35d2c777d6735a3b6b00e
chris <chris1ding1@gmail.com>
2025-07-13 12:56:49 +0800

    h1 hide

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
     <header class="mx-auto max-w-7xl text-center mt-18">
-      <h1 class="text-4xl font-medium tracking-tight text-gray-900 mb-10">
-        <span class="md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">{{ title }}</span>
+      <h1 class="sr-only">
+        {{ title }}
       </h1>
       <div class="mx-auto max-w-3xl mb-1">
         <AppSearch />
