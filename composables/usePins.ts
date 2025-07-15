bb3bf18c29508c57759ec0a269dd67fc0af8db58
chris <chris1ding1@gmail.com>
2025-07-15 23:50:38 +0800

    define verify type.

--- composables/usePins.ts
+++ composables/usePins.ts
@@ -1,3 +1,5 @@
+import * as v from 'valibot';
+
 export enum PinType {
   URL = "url",
   TEXT = "text",
@@ -26,6 +28,14 @@ export interface CreatePinData {
   visibility: PinVisibility;
 }
 
+export interface PinForm {
+    url: string;
+    content: string;
+    audio_path: string;
+    image_path: string;
+    visibility: PinVisibility;
+}
+
 export const PinVisibilityPublic = {
     id: "visibility-public",
     title: 'Public',
@@ -37,3 +47,74 @@ export const PinVisibilityPrivate = {
     title: 'Private',
     val: PinVisibility.PRIVATE
 }
+
+export const PinImageMaxSize = 5242880 // 5 MB
+
+export const pinUploadImage = async (file: File): Promise<FileUploadResponse | false> => {
+    const formData = new FormData();
+    formData.append('file', file);
+    try {
+        const response = await useNuxtApp().$api<FileUploadResponse>('/pins/upload-image', {
+            method: 'POST',
+            body: formData,
+        })
+        return response
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+export const ValidatorPinImageSchema = v.pipe(
+    v.file(),
+    v.mimeType(['image/jpeg', 'image/png'], 'Please select a JPEG or PNG file.'),
+    v.maxSize(PinImageMaxSize, 'Please select a file smaller than 5 MB.')
+)
+
+type ValidationPinErrorDetails = {
+    [K in keyof PinForm]: string[];
+};
+
+export interface ValidationPinErrors {
+    message: string;
+    details: ValidationPinErrorDetails;
+}
+
+export const ValidatorPinFormSchema = v.pipe(
+    v.object({
+      url: v.optional(
+        v.pipe(
+          v.string(),
+          v.nonEmpty(),
+          v.url(),
+          v.maxLength(2048)
+        ),
+      ),
+      content: v.optional(
+        v.pipe(
+          v.string(),
+          v.nonEmpty(),
+          v.maxLength(3000),
+        ),
+      ),
+      audio_path: v.optional(
+        v.pipe(
+          v.string(),
+          v.nonEmpty(),
+        ),
+      ),
+      image_path: v.optional(
+        v.pipe(
+          v.string(),
+          v.nonEmpty(),
+        ),
+      ),
+      visibility: v.pipe(
+        v.picklist([PinVisibility.PRIVATE, PinVisibility.PUBLIC]),
+      ),
+    }),
+    v.check(
+      (input) => Boolean(input.url || input.content || input.audio_path || input.image_path),
+      'Please enter either a URL, Content, or upload an image or audio.'
+    )
+  );
