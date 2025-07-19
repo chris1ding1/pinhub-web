bf129029610bc780d7366d2f65ba4eb82a3f96ce
chris <chris1ding1@gmail.com>
2025-07-19 17:14:26 +0800

    vueuse package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/i18n', 'nuxt-auth-utils', '@nuxt/icon'],
+  modules: ['@nuxt/eslint', '@nuxtjs/i18n', 'nuxt-auth-utils', '@nuxt/icon', '@vueuse/nuxt'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
